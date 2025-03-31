c9b8627207eb189b637ca9ea1d213c400c6662c6
limcaaarl <carljovi22@gmail.com>
2025-03-31 14:59:06 +0800

    Add more instruction types

--- src/compiler/Instruction.ts
+++ src/compiler/Instruction.ts
@@ -1,8 +1,15 @@
 export type Instruction =
     | { tag: "LDC"; val: any }
     | { tag: "LD"; sym: string }
+    | { tag: "LDF"; prms: any; addr: number }
     | { tag: "ASSIGN"; sym: string }
     | { tag: "JOF"; addr: number }
     | { tag: "GOTO"; addr: number }
+    | { tag: "UNOP"; sym: string }
+    | { tag: "BINOP"; sym: string }
+    | { tag: "CALL"; arity: number }
+    | { tag: "RESET"; }
+    | { tag: "ENTER_SCOPE"; syms: any }
+    | { tag: "EXIT_SCOPE"; syms: any }
     | { tag: "POP" }
     | { tag: "DONE" };
