5eba6e0f51f69826f0db43a16420e6457b82ce96
limcaaarl <carljovi22@gmail.com>
2025-04-10 00:03:35 +0800

    Add heap management Currently still buggy, most of the test cases still fail

---
+++ src/NodeTags.ts
@@ -0,0 +1,15 @@
+export const TAGS = {
+    False_tag: 0,
+    True_tag: 1,
+    Number_tag: 2,
+    Null_tag: 3,
+    Unassigned_tag: 4,
+    Undefined_tag: 5,
+    Blockframe_tag: 6,
+    Callframe_tag: 7,
+    Closure_tag: 8,
+    Frame_tag: 9,
+    Environment_tag: 10,
+    Pair_tag: 11,
+    Builtin_tag: 12,
+};
