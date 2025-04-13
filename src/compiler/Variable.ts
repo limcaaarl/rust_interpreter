e5a8642034cca0c42e54eb19a4025ab006861462
limcaaarl <carljovi22@gmail.com>
2025-04-13 21:14:44 +0800

    Add new interface for storing var info

---
+++ src/compiler/Variable.ts
@@ -0,0 +1,16 @@
+export interface Variable {
+    name: string;
+    isOwned: boolean; // false -> Value is moved, True -> Var still owns some value
+    borrow: Borrow.None | Borrow.Immutable | Borrow.Mutable;
+    immCount?: number; // number of simultaneous immutable borrows
+}
+
+export type Backup = Map<string, { borrow: Variable["borrow"], isOwned: Variable["isOwned"], immCount?: number }>;
+
+
+export enum Borrow {
+    None = "none",
+    Immutable = "immutable",
+    Mutable = "mutable",
+}
+  
